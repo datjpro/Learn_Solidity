68da92922de81037f8a97cdb82485ad4d4a18ccc
Tô Phạm Thành Đạt <todat2207@gmail.com>
2025-09-30 21:07:40 +0700

    memory array

--- array.sol
+++ array.sol
@@ -2,27 +2,34 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract myArray{
-    //storage array :
-    uint[] array1;
-
-
-    //memory array
+    //storage array
+    //memory array :được lưu trong funcion, chỉ đc khai báo ở trong function, khai báo độ dài
     //array as parameter
-
+    uint[] array1;
 
 
     function DoSomething()external{
         array1.push(2);
         array1.push(5);
         array1[1] = 9;
-
         delete array1[0];
-
         for (uint i = 0; i < array1.length ; i++){
             array1[i];
         }
-
-
     }
+
+    function bar() external{
+        uint[] memory array2 = new uint[](10);
+        array2[0] = 2;
+        array2[1] = 7;
+        delete array2[0];
+    }
+
+    function foo(uint[] calldata array3) external {
+        
+    }
+
+
+
 
 }
